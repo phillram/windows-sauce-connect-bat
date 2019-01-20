8e13e03da37d6ff1cb5d3fa57359734566bd26c0
unknown <14913130+phillram@users.noreply.github.com>
2019-01-20 10:34:09 -0800

    Modified OSX example to Windows style

--- Win_Sauce_Connect.bat
+++ Win_Sauce_Connect.bat
@@ -1,50 +1,10 @@
-#!/bin/bash
+start D:\Coding\sauce-connect\4.5.3\bin\sc.exe ^
+--user $SAUCE_USERNAME ^
+--api-key $SAUCE_ACCESS_KEY ^
+--tunnel-identifier="Phill Tunnel One" ^
+--no-remove-colliding-tunnels ^
+--se-port 0 ^
+--pidfile D:\Coding\sauce-connect\pid\tunnel1.pid ^
+--logfile D:\Coding\sauce-connect\logs\tunnel1.log
 
-ulimit -n 8000
-
-/Users/phillipramirez/sauce-connect/4.5.3/bin/sc \
-    --user $SAUCE_USERNAME \
-    --api-key $SAUCE_ACCESS_KEY \
-    --no-remove-colliding-tunnels \
-    --tunnel-identifier="Phill Tunnel One" \
-    --se-port 0 \
-    --pidfile /Users/phillipramirez/sauce-connect/pid/tunnel1.pid \
-    --logfile /Users/phillipramirez/sauce-connect/logs/tunnel1.log &
-
-/Users/phillipramirez/sauce-connect/4.5.3git /bin/sc \
-    --user $SAUCE_USERNAME \
-    --api-key $SAUCE_ACCESS_KEY \
-    --no-remove-colliding-tunnels \
-    --tunnel-identifier="Phill Tunnel Two" \
-    --se-port 0 \
-    --pidfile /Users/phillipramirez/sauce-connect/pid/tunnel2.pid \
-    --logfile /Users/phillipramirez/sauce-connect/logs/tunnel2.log &
-
-/Users/phillipramirez/sauce-connect/4.5.3/bin/sc \
-    --user $SAUCE_USERNAME \
-    --api-key $SAUCE_ACCESS_KEY \
-    --no-remove-colliding-tunnels \
-    --tunnel-identifier="Phill Tunnel Three" \
-    --se-port 0 \
-    --pidfile /Users/phillipramirez/sauce-connect/pid/tunnel3.pid \
-    --logfile /Users/phillipramirez/sauce-connect/logs/tunnel3.log &
-
-/Users/phillipramirez/sauce-connect/4.5.2/bin/sc \
-    --user $SAUCE_USERNAME \
-    --api-key $SAUCE_ACCESS_KEY \
-    --no-remove-colliding-tunnels \
-    --tunnel-identifier="Phill Tunnel Four" \
-    --se-port 0 \
-    --pidfile /Users/phillipramirez/sauce-connect/pid/tunnel4.pid \
-    --logfile /Users/phillipramirez/sauce-connect/logs/tunnel4.log &
-
-/Users/phillipramirez/sauce-connect/4.5.2/bin/sc \
-    --user $SAUCE_USERNAME \
-    --api-key $SAUCE_ACCESS_KEY \
-    --no-remove-colliding-tunnels \
-    --tunnel-identifier="Phill Tunnel Five" \
-    --se-port 0 \
-    --pidfile /Users/phillipramirez/sauce-connect/pid/tunnel5.pid \
-    --logfile /Users/phillipramirez/sauce-connect/logs/tunnel5.log &
-
-wait
+pause
